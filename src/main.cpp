2fb4e3af504cdfbdc80300a74cb8c01828a99ece
Sourec <tothemyers@gmail.com>
2019-10-04 16:13:53 -0400

    Add database config parsing code
    Honestly I just needed to push this to github so I had to commit what I
    had.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <pqxx/pqxx>
 #include "cmdline.h"
+
+using namespace std;
+using json = nlohmann::json;
 
 int main(int argc, char** argv) {
 	gengetopt_args_info args{};
 	cmdline_parser(argc, argv, &args);
+
+	// Load database parameters for connection
+	fstream dbconfig_file;
+	dbconfig_file.open(args.dbconfig_arg, ios::in);
+	if (!dbconfig_file.is_open()) {
+		cerr << "Failed to open database configuration in " << args.dbconfig_arg << endl;
+		return 1;
+	}
+	json dbconfig;
+	dbconfig_file >> dbconfig;
+	dbconfig_file.close();
 }
